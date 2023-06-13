9d391111b9878d2d7b825ca88775550e21d7991b
nurwachidaji034 <nugrahaaji402@gmail.com>
2023-06-13 10:20:51 +0800

    Membuat class Main dan menjalankan

---
+++ src/geometri/Main.java
@@ -0,0 +1,25 @@
+package geometri;
+
+public class Main {
+     public static void main(String[] args) {
+        // Membuat objek Lingkaran
+        Lingkaran lingkaran = new Lingkaran(5.0);
+        double luasLingkaran = lingkaran.luas();
+        double kelilingLingkaran = lingkaran.keliling();
+
+        System.out.println("Lingkaran:");
+        System.out.println("Luas: " + luasLingkaran);
+        System.out.println("Keliling: " + kelilingLingkaran);
+
+        System.out.println();
+
+        // Membuat objek PersegiPanjang
+        PersegiPanjang persegiPanjang = new PersegiPanjang(4.0, 6.0);
+        double luasPersegiPanjang = persegiPanjang.luas();
+        double kelilingPersegiPanjang = persegiPanjang.keliling();
+
+        System.out.println("Persegi Panjang:");
+        System.out.println("Luas: " + luasPersegiPanjang);
+        System.out.println("Keliling: " + kelilingPersegiPanjang);
+    }
+}
